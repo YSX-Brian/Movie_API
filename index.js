60976d5e0e61c8d59b8e7d9cace437d57c921f16
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-07-24 21:16:15 -0400

    update / endpoint

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ app.get('/documentation.html', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Welcome to myFlix- an application about movies. Create a profile, browse our movie database, and save your favorites. Learn more at https://myflix17507.herokuapp.com/documentation.html');
+  res.send('Welcome to myFlix- an application about movies. Create a profile, browse our movie database, and save your favorites. Learn more at:  https://myflix17507.herokuapp.com/documentation.html');
 });
 
 //for errors
