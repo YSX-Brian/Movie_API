b99ace6eecf1b68ea0541f1c40760594463c587f
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-08-28 23:23:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
 });
 
 // Add a movie to a user's list of favorites
-app.post('/users/:Username/movies/:MovieID', (req, res) => {
+app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, {
         $push: { FavoriteMovies: req.params.MovieID }
     },
