f380b66afe6672ced286cb7d405bf407d70a9b6d
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-18 16:35:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.static('public'));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix17507.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix17507.netlify.app', 'http://localhost:4200'];
 
 app.use(cors({
     origin: (origin, callback) => {
