95987922c230830d28c1df601641bfcff87e7094
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-18 17:26:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,16 +28,18 @@ const cors = require('cors');
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix17507.netlify.app', 'http://localhost:4200'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+
+app.use(cors())
 
 let auth = require('./auth')(app);
 const passport = require('passport');
