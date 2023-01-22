16933e59371980c9823f24fb57a6fb982ad47de8
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2023-01-22 01:24:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.static('public'));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix17507.netlify.app', 'http://localhost:4200', 'https://megidolaonn.github.io/myFlix-Angular-client'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix17507.netlify.app', 'http://localhost:4200', 'https://megidolaonn.github.io'];
 
 app.use(cors({
     origin: (origin, callback) => {
